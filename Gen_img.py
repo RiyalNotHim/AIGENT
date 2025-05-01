ac3c34de93c993cfbbb9666b580cab57b2e399be
RiyalNotHim <csattyam@gmail.com>
2025-05-01 15:23:31 +0530

    Update Gen_img.py

--- Gen_img.py
+++ Gen_img.py
@@ -3,7 +3,7 @@ import openai as op
 import pyautogui
 
 def gen_image():
-    op.api_key = "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"
+    op.api_key = ""
     user_input = input("Write you prompt Here:")
 
     respone = op.Image.create(
